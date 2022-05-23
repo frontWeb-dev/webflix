3e71f67a71cd1551cb17ce2011021c598400307e
dev902 <frontend_web@kakao.com>
2022-05-24 03:50:21 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // style
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import TV from './Routes/TV';
+import Search from './Routes/Search';
 
 function App() {
-  return <></>;
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path='movies/:id' element={<Home />} />
+        </Route>
+        <Route path='tv' element={<TV />} />
+        <Route path='search' element={<Search />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
